d3b3b4257f47c235ae0efddf8536226589e15bc3
NiceShao <shaohao1993@github.com>
2017-11-03 22:46:55 +0800

    blog test version 1.1

---
+++ myblog/blog/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+#from django.shortcuts import render
+#from blog.models import BlogsPost
+#from django.shortcuts import render_to_response
+from .models import *
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    blog_list = BlogsPost.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'blog_list':blog_list
+    }
+    return HttpResponse(template.render(context, request))
